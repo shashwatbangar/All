95da63506b2d8127e5031cf7d1a0f7f6926ecfe5
shashwatbangar <62360781+shashwatbangar@users.noreply.github.com>
2020-10-03 13:47:37 +0530

    Add files via upload
    SImon Game

---
+++ Simon game/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,58 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var started = false;
+ var level = 0;
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+});
+
+$(document).keypress(function(){
+  if(!started){
+
+    $("#level-title").text("Level " +level);
+      nextSequence();
+      started = true;
+  }
+});
+
+
+function nextSequence() {
+  level++;
+
+  $("#level-title").text("Level " +level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+
+  $( "#" + currentColour).addClass("pressed");
+
+
+ setTimeout(function (){
+   $("#" +currentColour).removeClass("pressed")
+ },100);
+}
